fa186e52e78054a420d71947b3454f2854ad0289
sh-wx <130127239+sh-wx@users.noreply.github.com>
2023-04-07 13:38:19 +0800

    Add files via upload

---
+++ outputNodeElement.py
@@ -0,0 +1,29 @@
+from abaqus import *
+from abaqusConstants import *
+from caeModules import *
+from driverUtils import executeOnCaeStartup
+from numpy import *
+import regionToolset
+import numpy as np
+import os
+import time
+import shutil
+import math
+def main(modelname,instancename,nodeY,elementY,filen,filel):  
+    lnode=len(mdb.models[modelname].rootAssembly.instances[instancename].nodes)
+    nodematrix = np.zeros((lnode, 4))
+    b=mdb.models[modelname].rootAssembly.instances[instancename]
+    for i in range(lnode):
+        nodematrix[i, :] =np.hstack((b.nodes[i].label,b.nodes[i].coordinates))
+    if nodeY==True:
+        np.savetxt(filen, np.c_[nodematrix],fmt='%f',delimiter='\t')
+        print('The node file has been output')
+    if elementY==True:
+        lelement=len(b.elements)
+        esize=size(b.elements[0].connectivity)
+        elementmatrix=np.zeros((lelement, esize+1))
+        for i in range(lelement):
+            elementmatrix[i, :] =np.hstack((b.elements[i].label,b.elements[i].connectivity))
+        np.savetxt(filel, np.c_[elementmatrix],fmt='%f',delimiter='\t')
+        print('The element file has been output')
+    print('All done! Enjoy!')
---
+++ outputNodeElement_plugin.py
@@ -0,0 +1,19 @@
+from abaqusGui import getAFXApp, Activator, AFXMode
+from abaqusConstants import ALL
+import os
+thisPath = os.path.abspath(__file__)
+thisDir = os.path.dirname(thisPath)
+
+toolset = getAFXApp().getAFXMainWindow().getPluginToolset()
+toolset.registerGuiMenuButton(
+    buttonText='Output Node&Element', 
+    object=Activator(os.path.join(thisDir, 'outputNodeElementDB.py')),
+    kernelInitString='import outputNodeElement',
+    messageId=AFXMode.ID_ACTIVATE,
+    icon=None,
+    applicableModules=ALL,
+    version='N/A',
+    author='N/A',
+    description='N/A',
+    helpUrl='N/A'
+)
