d2818df53994810222ca051a89ea42b29f58d258
sh-wx <130127239+sh-wx@users.noreply.github.com>
2023-05-19 19:51:25 +0800

    Add files via upload
    现在可以输出set了，加入了控件状态选项，使用set时需要在assembl里建nodeset和elementset

--- outputNodeElement.py
+++ outputNodeElement.py
@@ -9,22 +9,39 @@ import os
 import time
 import shutil
 import math
-def main(modelname,instancename,nodeY,elementY,filen,filel):  
-    lnode=len(mdb.models[modelname].rootAssembly.instances[instancename].nodes)
-    nodematrix = np.zeros((lnode, 4))
-    b=mdb.models[modelname].rootAssembly.instances[instancename]
-    for i in range(lnode):
-        nodematrix[i, :] =np.hstack((b.nodes[i].label,b.nodes[i].coordinates))
-    if nodeY==True:
-        np.savetxt(filen, np.c_[nodematrix],fmt='%f',delimiter='\t')
-        print('The node file has been output')
-    if elementY==True:
-        lelement=len(b.elements)
-        esize=size(b.elements[0].connectivity)
-        elementmatrix=np.zeros((lelement, esize+1))
-        for i in range(lelement):
-            elementmatrix[i, :] =np.hstack((b.elements[i].label,b.elements[i].connectivity))
-        elementmatrix[:,1:]=elementmatrix[:,1:]+1
-        np.savetxt(filel, np.c_[elementmatrix],fmt='%f',delimiter='\t')
-        print('The element file has been output')
+def main(modelname,instancename,nodeY,elementY,filen,filel,useset,nodeset,elementset):  
+    if useset==True:
+        nodesett=mdb.models[modelname].rootAssembly.sets[nodeset]
+        nodematrix = np.zeros((len(nodesett.nodes), 4))
+        for i in range(len(nodesett.nodes)):
+            nodematrix[i, :] =np.hstack((nodesett.nodes[i].label,nodesett.nodes[i].coordinates))
+        elementsett=mdb.models[modelname].rootAssembly.sets[elementset]
+        esize=size(elementsett.elements[0].connectivity)
+        elementmatrix=np.zeros((len(elementsett.elements), esize+1))
+        for i in range(len(elementsett.elements)):
+            elementmatrix[i, :] =np.hstack((elementsett.elements[i].label,elementsett.elements[i].connectivity))
+        if nodeY==True:
+            np.savetxt(filen, np.c_[nodematrix],fmt='%f',delimiter='\t')
+            print('The node file has been output')
+        if elementY==True:
+            np.savetxt(filel, np.c_[elementmatrix],fmt='%f',delimiter='\t')
+            print('The element file has been output')
+    else:
+        lnode=len(mdb.models[modelname].rootAssembly.instances[instancename].nodes)
+        nodematrix = np.zeros((lnode, 4))
+        b=mdb.models[modelname].rootAssembly.instances[instancename]
+        for i in range(lnode):
+            nodematrix[i, :] =np.hstack((b.nodes[i].label,b.nodes[i].coordinates))
+        if nodeY==True:
+            np.savetxt(filen, np.c_[nodematrix],fmt='%f',delimiter='\t')
+            print('The node file has been output')
+        if elementY==True:
+            lelement=len(b.elements)
+            esize=size(b.elements[0].connectivity)
+            elementmatrix=np.zeros((lelement, esize+1))
+            for i in range(lelement):
+                elementmatrix[i, :] =np.hstack((b.elements[i].label,b.elements[i].connectivity))
+            elementmatrix[:,1:]=elementmatrix[:,1:]+1
+            np.savetxt(filel, np.c_[elementmatrix],fmt='%f',delimiter='\t')
+            print('The element file has been output')
     print('All done! Enjoy!')
--- outputNodeElement_plugin.py
+++ outputNodeElement_plugin.py
@@ -1,16 +1,76 @@
-from abaqusGui import getAFXApp, Activator, AFXMode
+from abaqusGui import *
 from abaqusConstants import ALL
-import os
+import osutils, os
+
+
+###########################################################################
+# Class definition
+###########################################################################
+
+class OutputNodeElement_plugin(AFXForm):
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def __init__(self, owner):
+        
+        # Construct the base class.
+        #
+        AFXForm.__init__(self, owner)
+        self.radioButtonGroups = {}
+
+        self.cmd = AFXGuiCommand(mode=self, method='main',
+            objectName='outputNodeElement', registerQuery=False)
+        pickedDefault = ''
+        self.modelnameKw = AFXStringKeyword(self.cmd, 'modelname', True, 'Model-1')
+        self.nodesetKw = AFXStringKeyword(self.cmd, 'nodeset', True, 'NSet-1')
+        self.instancenameKw = AFXStringKeyword(self.cmd, 'instancename', True, 'Part-1-1')
+        self.elementsetKw = AFXStringKeyword(self.cmd, 'elementset', True, 'ESet-1')
+        self.usesetKw = AFXBoolKeyword(self.cmd, 'useset', AFXBoolKeyword.TRUE_FALSE, True, False)
+        self.nodeYKw = AFXBoolKeyword(self.cmd, 'nodeY', AFXBoolKeyword.TRUE_FALSE, True, True)
+        self.filenKw = AFXStringKeyword(self.cmd, 'filen', True, 'node.txt')
+        self.elementYKw = AFXBoolKeyword(self.cmd, 'elementY', AFXBoolKeyword.TRUE_FALSE, True, True)
+        self.filelKw = AFXStringKeyword(self.cmd, 'filel', True, 'element.txt')
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def getFirstDialog(self):
+
+        import outputNodeElementDB
+        return outputNodeElementDB.OutputNodeElementDB(self)
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def doCustomChecks(self):
+
+        # Try to set the appropriate radio button on. If the user did
+        # not specify any buttons to be on, do nothing.
+        #
+        for kw1,kw2,d in self.radioButtonGroups.values():
+            try:
+                value = d[ kw1.getValue() ]
+                kw2.setValue(value)
+            except:
+                pass
+        return True
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def okToCancel(self):
+
+        # No need to close the dialog when a file operation (such
+        # as New or Open) or model change is executed.
+        #
+        return False
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Register the plug-in
+#
 thisPath = os.path.abspath(__file__)
 thisDir = os.path.dirname(thisPath)
 
 toolset = getAFXApp().getAFXMainWindow().getPluginToolset()
 toolset.registerGuiMenuButton(
     buttonText='Output Node&Element', 
-    object=Activator(os.path.join(thisDir, 'outputNodeElementDB.py')),
-    kernelInitString='import outputNodeElement',
+    object=OutputNodeElement_plugin(toolset),
     messageId=AFXMode.ID_ACTIVATE,
     icon=None,
+    kernelInitString='import outputNodeElement',
     applicableModules=ALL,
     version='N/A',
     author='N/A',
